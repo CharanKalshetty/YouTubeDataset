39937e53f9907f55e4d714d93eedcc3dfb227cfc
CharanKalshetty <charan1649kalshetty@gmail.com>
2020-07-24 17:24:23 +0530

    improved

--- Program.py
+++ Program.py
@@ -1,176 +1,97 @@
-#importing required libraries
+# importing packages
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
 
 
-#importing text file as dataframe using pandas
-df = pd.read_csv("youtubedata.txt",sep='\t')
-
-
-#storing all related video ids in a single column and resizing data frame
-print(df.shape)
-l=[]
-for i in range(0,4100):
-    l0=[df.ix[i,j] for j in range(10,29)]
-    l.append(list(l0))
-df=df.ix[0:4099,0:9]
-df['REL_VID_ID']=l
-print(df['REL_VID_ID'])
-
-
-#renaming columns
-print(list(df))
-df.columns = ['VID_ID', 'UPLOADER', 'INT_AFT_EST(m)', 'CATEGORY', 'LENGTH(m)', 'NO_OF_VIEWS', 'RATINGS(/5)', 'NO_OF_RATS', 'NO_OF_COMS', 'REL_VID_ID']
-print(list(df))
+# importing data into DataFrame
+df=pd.read_csv('preprocessed.csv')
+print("The columns in DataFrame are: ",list(df),'\n')
 
 
 #Question 1) Identify top 10 videos with highest ratings
-z1 = df.sort_values(by='RATINGS(/5)', ascending=0)
-z2 = z1.head(10)
-print("\nThe top 10 videos with highest ratings are stored in 'top10rats.txt'\n\n\n")
-z2.to_csv('top10rats.csv')
+tdf = df.sort_values(by='rating', ascending=0)
+tdf = tdf.head(10)
+print("The top 10 videos with highest ratings are stored in 'top10videosbyrating.csv'\n")
+tdf.to_csv('top10videosbyrating.csv')
 
 
 #Question 2) Identify top 5 categories with highest ratings
-d={}
-for i in df['CATEGORY'].unique():
-    avg_rat=0
-    if i not in d.keys():
-        y1=df[df['CATEGORY']==i]
-        l1 = y1['RATINGS(/5)']
-        for j in l1:
-            avg_rat+=j
-        try:
-            avg_rat=avg_rat/len(l1)
-        except ZeroDivisionError:
-            avg_rat=0
-        d.update({i:avg_rat})
-sorted_d=sorted(d.items(),key=lambda kv:kv[1],reverse=True)
-l21=[i[0] for i in sorted_d]
-l22=[i[1] for i in sorted_d]
-data = {'Category':l21,'Avg_Ratings':l22}
-y2 = pd.DataFrame(data)
-y3=y2.head()
-#print("Top 5 categories with highest ratings are\n")
-#print(y3)
-print("The top 5 Categories with maximum ratings are stored in 'top5cats.csv'")
-y3.to_csv('top5cats.csv')
-print("\n\n\n")
-
-        
-#Question 3) Identify top 10 videos with maximum video time(i.e Length)
-x1 = df.sort_values(by='LENGTH(m)', ascending=0)
-x2 = x1.head(10)
-print("The top 10 videos with maximum video length are stored in 'top10lens.csv'\n\n\n")
-x2.to_csv('top10lens.csv')
-
-'''
-#Question 4)Most controversial video categories based on no. of comments by no. of views
-di={}
-for i in df['CATEGORY'].unique():
-    avg_com_score=0
-    if i not in di.keys():
-        w1=df[df['CATEGORY']==i]
-        l31=np.array(w1['NO_OF_COMS'])
-        l32=np.array(w1['NO_OF_VIEWS'])
-        l33=np.ma.array(l31,mask=(l31==0))
-        l34=np.ma.array(l32,mask=(l32==0))
-        l3=l33/l34
-        l3=[i*1000 for i in l3]
-        for n in l3:
-            avg_com_score+=n
-        try:
-            avg_com_score/=len(l3)
-        except ZeroDivisionError:
-            avg_com_score=0
-        di.update({i:avg_com_score})
-sorted_d=sorted(di.items(),key=lambda kx:kx[1],reverse=True)
-w2=pd.DataFrame(sorted_d)
-w2.columns=['Category', 'Con_Score']
-print("Categories arranged in the order of  controversiality are stored in 'topconcats.csv'")
-w2.to_csv('topconcats.csv')
-print("\n\n\n")
-'''   
-        
-#Question 5)calculate popularity of each video and rank them from best to worst
-l41=np.ma.array(df['NO_OF_VIEWS'],mask=(df['NO_OF_VIEWS']==0))
-l42=np.ma.array(df['INT_AFT_EST(m)'],mask=(df['INT_AFT_EST(m)']==0))
-l4=l41/l42
-l51=np.ma.array(df['RATINGS(/5)'],mask=(df['RATINGS(/5)']==0))
-l52=np.ma.array(df['NO_OF_RATS'],mask=(df['NO_OF_RATS']==0))
-l5=l51/l52
-l6=np.ma.array(df['NO_OF_COMS'],mask=(df['NO_OF_COMS']==0))
-l7=l4+l5+l6
-df['POPULARITY']=l7
-v=df.sort_values(by='POPULARITY', ascending=0)
-print("The data sorted with respect to popularity is stored in 'sortedwithpop.csv'")
-v.to_csv('sortedwithpop.csv')
-print("The popularity was calculated by no. of views/interval after publish + rating out of 5/no. of ratings + no. of comments\n\n")
+def categorisedData(s): # function to find average value of 's' for each category
+    d={}
+    for i in df['category'].unique():
+        avg=0
+        if i not in d.keys():
+            tdf=df[df['category']==i]
+            l = tdf[s]
+            for j in l:
+                avg +=j
+            try:
+                avg/=len(l)
+            except ZeroDivisionError:
+                avg=0
+            d.update({i:avg})
+    sorted_d=sorted(d.items(),key=lambda kv:kv[1],reverse=True)
+    tdf = pd.DataFrame(sorted_d)
+    return tdf
+tdf=categorisedData('rating')
+tdf.columns=['Category', 'Avg Rating (/5)']
+print("Categories with their average ratings (out of 5) are\n")
+print(tdf,'\n')
+print("The Categories with their average ratings are stored in 'categoriesbyrating.csv' \n")
+tdf.to_csv('categoriesbyrating.csv')
 
 
-#trying clustering and finding relation between length of video and no. of views.
-y=list(df['LENGTH(m)'])
-z=list(df['NO_OF_VIEWS'])
-x=[]
-for i in range(len(y)):
-    w1=[]
-    if y[i] != 'nan' and z[i] != 'nan':
-        if y[i]<=np.finfo(np.float64).max and z[i]<=np.finfo(np.float64).max:
-            w1.append(y[i])
-            w1.append(z[i])
-            x.append(list(w1))
-x=np.array(x)
-#plt.subplot(2,2,1)
-#plt.scatter(x[:,0],x[:,1], label='True Position')
-kmeans=KMeans(n_clusters=3)
-kmeans.fit(x)
-print("The cluster centers are as follows")
-l91=kmeans.cluster_centers_
-for i in l91:
-    print(float(i[0]),float(i[1]))
-plt.subplot(1,1,1)
-plt.scatter(x[:, 0], x[:, 1], c=kmeans.labels_)
-plt.scatter(kmeans.cluster_centers_[:,0] ,kmeans.cluster_centers_[:,1], color='black')
-plt.title("Relation between Length of video and no. of views")
-plt.xlabel("Length in mins")
-plt.ylabel("no. of Views")
-plt.show()
-print('\n\n')
+#Question 3)Most controversial video categories based on no. of comments by no. of views
+l1=np.ma.array(df['comments-cnt'],mask=(df['comments-cnt']==0))
+l2=np.ma.array(df['views-cnt'],mask=(df['views-cnt']==0))
+l=l1/l2*1000
+df['controversial-score']=l
+tdf=categorisedData('controversial-score')
+tdf.columns=['Category', 'Avg Controversial Score']
+print("Categories with their average Controversial Scores are\n")
+print(tdf,'\n')
+print("The Categories with their average Controversial Scores are stored in 'categoriesbycontroscore.csv' \n")
+tdf.to_csv('categoriesbycontroscore.csv')
 
 
-#categories with highest popularity
-di={}
-for i in df['CATEGORY'].unique():
-    avg_pop_score=0
-    if i not in di.keys():
-        w1=df[df['CATEGORY']==i]
-        l3=np.array(w1['POPULARITY'])
-        for n in l3:
-            avg_pop_score+=n
-        try:
-            avg_com_score/=len(l3)
-        except ZeroDivisionError:
-            avg_pop_score=0
-        di.update({i:avg_pop_score})
-sorted_d=sorted(di.items(),key=lambda kx:kx[1],reverse=True)
-w2=pd.DataFrame(sorted_d)
-w2.columns=['Category', 'Pop_Score']
-print("Categories arranged in the order of  popularity are stored in 'toppopcats.csv'")
-w2.to_csv('toppopcats.csv')
-print("\n\n\n")
+#Question 4)calculate popularity of each video and rank them from best to worst
+tl1=np.ma.array(df['views-cnt'],mask=(df['views-cnt']==0))
+tl2=np.ma.array(df['time-from-upload'],mask=(df['time-from-upload']==0))
+l1=tl1/tl2
+tl1=np.ma.array(df['rating'],mask=(df['rating']==0))
+tl2=np.ma.array(df['ratings-cnt'],mask=(df['ratings-cnt']==0))
+l2=tl1*tl2
+l3=np.ma.array(df['comments-cnt'],mask=(df['comments-cnt']==0))
+l=(l1+l2+l3)/1000
+df['POPULARITY']=l
+df=df.sort_values(by='POPULARITY', ascending=0)
+print("The data sorted with respect to popularity is stored in 'sortedbyypopularity.csv'\n")
+df.to_csv('sortedbypopularity.csv')
+print("The popularity was calculated by (no. of views/interval after publish + rating out of 5*no. of ratings + no. of comments) by 1000\n")
 
 
-#Identify top 10 videos with maximum no. of views
-x1 = df.sort_values(by='NO_OF_VIEWS', ascending=0)
-x2 = x1.head(10)
-print("The top 10 videos with maximum no. of views are stored in 'top10views.csv'\n\n\n")
-x2.to_csv('top10views.csv')
+#Question 5) plot graph between related videos count and views count and print conclusion
+def plotScatterPlot(sx, sy):
+    l1=list(df[sx])
+    l2=list(df[sy])
+    x=[]
+    for i in range(len(l1)):
+        l=[]
+        l.append(l1[i])
+        l.append(l2[i])
+        x.append(list(l))
+    x=np.array(x)
+    plt.scatter(x[:, 0], x[:, 1])
+    plt.title("Relation between "+sx+" and "+sy)
+    plt.xlabel(sx)
+    plt.ylabel(sy)
+    plt.show()
+    
+plotScatterPlot('related-vids-cnt', 'views-cnt')
+print('The conclusion, after analysing the scatter plot between no. of related videos and no. of views, is that More the no. of related videos a video has, more could be its no. of views.\n')
 
 
-#Identify top 10 videos with maximum no. of comments
-x1 = df.sort_values(by='NO_OF_COMS', ascending=0)
-x2 = x1.head(10)
-print("The top 10 videos with maximum no. of comments are stored in 'top10coms.csv'\n\n\n")
-x2.to_csv('top10coms.csv')
+# plotting graph btw length and no. of views 
+plotScatterPlot('length', 'views-cnt')
+print('The conclusion, after analysing the scatter plot between length of video and no. of views, is that Lesser the length of video, more could be the no. of views.\n')
